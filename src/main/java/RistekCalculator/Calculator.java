cc705726e2280651141d3919bfc3aa4a7fecbdff
Muhammad Farabi <muhammad.farabi@go-jek.com>
2019-11-02 17:35:43 +0700

    [Abi|Fahriza] Reformat

--- src/main/java/RistekCalculator/Calculator.java
+++ src/main/java/RistekCalculator/Calculator.java
@@ -2,10 +2,10 @@ package RistekCalculator;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
-        return a-b;
+        return a - b;
     }
 }
